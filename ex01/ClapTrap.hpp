2b5fe0c365191a236d59876b2c0e6d25e63d05b0
akyoshid <akyoshid@student.42tokyo.jp>
2025-07-18 13:25:01 +0000

    [ADD] required files

---
+++ ex01/ClapTrap.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akyoshid <akyoshid@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/18 11:13:04 by akyoshid          #+#    #+#             */
+/*   Updated: 2025/07/18 13:05:31 by akyoshid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EX01_CLAPTRAP_HPP_
+# define EX01_CLAPTRAP_HPP_
+
+# include <string>
+
+class ClapTrap {
+ public:
+    ClapTrap();
+    explicit ClapTrap(const std::string& name);
+    ClapTrap(const ClapTrap& src);
+    ClapTrap& operator=(const ClapTrap& src);
+    ~ClapTrap();
+    void attack(const std::string& target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
+ private:
+    std::string name;
+    unsigned int hit_points;
+    unsigned int energy_points;
+    unsigned int attack_damage;
+};
+
+#endif
