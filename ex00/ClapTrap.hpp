11d05d50d3c95aaf20b78b44c758b43d524161e0
akyoshid <akyoshid@student.42tokyo.jp>
2025-07-18 13:03:29 +0000

    [SAVE] done ex00

--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -6,11 +6,30 @@
 /*   By: akyoshid <akyoshid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/18 11:13:04 by akyoshid          #+#    #+#             */
-/*   Updated: 2025/07/18 11:13:32 by akyoshid         ###   ########.fr       */
+/*   Updated: 2025/07/18 12:40:33 by akyoshid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EX00_CLAPTRAP_HPP_
 # define EX00_CLAPTRAP_HPP_
+
+# include <string>
+
+class ClapTrap {
+ public:
+    ClapTrap();
+    explicit ClapTrap(const std::string& name);
+    ClapTrap(const ClapTrap& src);
+    ClapTrap& operator=(const ClapTrap& src);
+    ~ClapTrap();
+    void attack(const std::string& target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
+ private:
+    std::string name;
+    unsigned int hit_points;
+    unsigned int energy_points;
+    unsigned int attack_damage;
+};
 
 #endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,8 +6,44 @@
 /*   By: akyoshid <akyoshid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/18 11:13:42 by akyoshid          #+#    #+#             */
-/*   Updated: 2025/07/18 11:13:50 by akyoshid         ###   ########.fr       */
+/*   Updated: 2025/07/18 13:00:50 by akyoshid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
+
+int main() {
+    ClapTrap shin("Shin");
+    ClapTrap houken("Houken");
+    ClapTrap shin2(shin);
+    ClapTrap houken2;
+    houken2 = houken;
+    houken.attack("Shin");
+    houken.attack("Shin");
+    houken.attack("Shin");
+    houken.attack("Shin");
+    houken.attack("Shin");
+    houken.attack("Shin");
+    houken.attack("Shin");
+    houken.attack("Shin");
+    houken.attack("Shin");
+    houken.attack("Shin");
+    houken.attack("Shin");
+    shin.takeDamage(10);
+    shin.takeDamage(1);
+    shin.beRepaired(10);
+    shin.attack("Houken");
+    shin2.takeDamage(9);
+    shin2.beRepaired(1);
+    shin2.beRepaired(1);
+    shin2.beRepaired(1);
+    shin2.beRepaired(1);
+    shin2.beRepaired(1);
+    shin2.beRepaired(1);
+    shin2.beRepaired(1);
+    shin2.beRepaired(1);
+    shin2.beRepaired(1);
+    shin2.beRepaired(1);
+    shin2.beRepaired(1);
+    return 0;
+}
